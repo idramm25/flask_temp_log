70fb18f5b2206eee4f14800979d54fbb3b54ffb6
root <lawapiwa@gmail.com>
2020-11-20 20:45:42 +0200

    New chart1.html design

--- app.py
+++ app.py
@@ -1,25 +1,24 @@
 # flask1
+# import os
 
 from flask import Flask, render_template, jsonify
 from flask import send_from_directory
 from flask_sqlalchemy import *
-from conn import connect
-from flask_breadcrumbs import Breadcrumbs, register_breadcrumb
-from time import strftime
+from bs4 import BeautifulSoup
+import time
+import requests
 
 from mysqltojson import getjson
 from mysqltocsv import mysql_to_csv
 
 from flaskr import *
 from apscheduler.scheduler import Scheduler
-from connSettings import *
 
 app = Flask(__name__, static_url_path='/static')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin-mysql25@localhost/log_temp_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["CLIENT_JSON"] = "/home/smart-server/projects/flask2/api/get-json"
 db = SQLAlchemy(app)
-Breadcrumbs(app=app)
 
 """-----------------------------------------------------------------------------------------------"""
 # Start the scheduler
@@ -28,23 +27,16 @@ sched.start()
 
 
 # Schedule job_function to be called every two hours
-@sched.interval_schedule(minutes=COUNT)  # logging ratio "minutes"
+@sched.interval_schedule(minutes=30)
 def sensor():
-    data = SensorsData(url=URL)  # Sensor URL
-    data.getparse()
-    print(strftime('%H:%M %d/%m/%Y') + " Getting data from sensor's")
+    getparse()
+    print(time.strftime('%H:%M %d/%m/%Y') + " Getting data from sensor's")
+
+
+"""-----------------------------------------------------------------------------------------------"""
 
 
 class Temp(db.Model):
-    def __init__(self, in_temp, out_temp, heat_temp, heating, light, pub_time, pub_date):
-        self.in_temp = in_temp
-        self.out_temp = out_temp
-        self.heat_temp = heat_temp
-        self.heating = heating
-        self.light = light
-        self.pub_time = pub_time
-        self.pub_date = pub_date
-
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     in_temp = db.Column(db.String(5), unique=False)
     out_temp = db.Column(db.String(5), unique=False)
@@ -54,31 +46,59 @@ class Temp(db.Model):
     pub_time = db.Column(db.String(2), unique=False)
     pub_date = db.Column(db.String(10), unique=False)
 
+    def __init__(self, in_temp, out_temp, heat_temp, heating, light, pub_time, pub_date):
+        self.in_temp = in_temp
+        self.out_temp = out_temp
+        self.heat_temp = heat_temp
+        self.heating = heating
+        self.light = light
+        self.pub_time = pub_time
+        self.pub_date = pub_date
+
 
 db.create_all()
+"""-----------------------------------------------------------------------------"""
 
 
-class SensorsData:
-    def __init__(self, url):
-        self.url = url
+def get_html(url):
+    r = requests.get(url)  # Получаем метод Response
+    r.encoding = 'utf8'  # У меня были проблемы с кодировкой, я задал в ручную
+    r.encoding = 'utf8'
+    return r.text
 
-    def getparse(self):
-        try:
-            intemp, outtemp = connect(self.url)
-            t3 = 0
-            h1 = 0
-            l1 = 0
-            tm = strftime('%H')
-            td = strftime('%d/%m/%Y')
-            db.session.add(Temp(intemp, outtemp, t3, h1, l1, tm, td))
-            db.session.commit()
-        except ConnectionError as e:
-            print(e)
 
-    def getcurrent(self):
+def get_head(html):
+    soup = BeautifulSoup(html, 'lxml')
+    head = soup.find_all('b')
+    heads = []
+    for i in head:
+        heads.append(i.string)
+    return heads
 
-        intemp, outtemp = connect(URL)
-        return intemp, outtemp, self.url
+
+def getparse():
+    intemp, outtemp = get_head(get_html('http://192.168.1.155'))
+    # t1 = intemp
+    # t2 = outtemp
+    t3 = 0
+    h1 = 0
+    l1 = 0
+    tm = time.strftime('%H')
+    td = time.strftime('%d/%m/%Y')
+
+    db.session.add(Temp(intemp, outtemp, t3, h1, l1, tm, td))
+    db.session.commit()
+
+
+def getcurrent():
+    url = 'http://192.168.1.155'
+    try:
+        data = get_head(get_html(url))
+        intemp, outtemp = data
+        return intemp, outtemp, url
+    except:
+        print("offline")
+        return None, None, url
 
 
 # -----------------------------------------------------------------------------"""
@@ -89,7 +109,6 @@ def favicon():
 
 
 @app.route('/')
-@register_breadcrumb(app, '.', 'Home')
 def index():
     return render_template('index.html')
 
@@ -101,16 +120,19 @@ def jsonget(json_id):
 
 
 @app.route('/chart')
-@register_breadcrumb(app, '.chart', 'Chart')
 def chart():
     return render_template('chart.html')
 
 
+@app.route('/iframe')
+def iframe():
+    intemp, outtemp, url = getcurrent()
+    return render_template('iframe.html', intemp=intemp, outtemp=outtemp, url=url)
+
+
 @app.route('/chart1')
-@register_breadcrumb(app, '.chart1', 'Chart1')
 def chart1():
-    data = SensorsData(url=URL)  # Sensor URL
-    intemp, outtemp, url = data.getcurrent()
+    intemp, outtemp, url = getcurrent()
     return render_template('chart1.html', intemp=intemp, outtemp=outtemp, url=url)
 
 
